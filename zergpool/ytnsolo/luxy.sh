fe62e8c1d1883ab9c4d32212cfaae28458083976
bibirbusin <132418678+bibirbusin@users.noreply.github.com>
2023-10-22 22:56:16 +0700

    Create luxy.sh

---
+++ zergpool/ytnsolo/luxy.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.8.0/SRBMiner-Multi-0-8-0-Linux.tar.xz
+tar -xvf SRBMiner-Multi-0-8-0-Linux.tar.xz
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerr16 --pool stratum+tcp://yespowerR16.mine.zergpool.com:6534 --wallet RQ9cLzR9o1E92Pw9q5LtiHvDXBfeWhqvgo --password c=RVN,mc=YTN,m=solo,ID=luxy$RANDOM --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerr16 --pool stratum+tcp://yespowerR16.mine.zergpool.com:6534 --wallet RQ9cLzR9o1E92Pw9q5LtiHvDXBfeWhqvgo --password c=RVN,mc=YTN,m=solo,ID=luxy$RANDOM --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerr16 --pool stratum+tcp://yespowerR16.mine.zergpool.com:6534 --wallet RQ9cLzR9o1E92Pw9q5LtiHvDXBfeWhqvgo --password c=RVN,mc=YTN,m=solo,ID=luxy$RANDOM --cpu-threads 1
+sleep 21600
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerr16 --pool stratum+tcp://yespowerR16.mine.zergpool.com:6534 --wallet RQ9cLzR9o1E92Pw9q5LtiHvDXBfeWhqvgo --password c=RVN,mc=YTN,m=solo,ID=luxy$RANDOM --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerr16 --pool stratum+tcp://yespowerR16.mine.zergpool.com:6534 --wallet RQ9cLzR9o1E92Pw9q5LtiHvDXBfeWhqvgo --password c=RVN,mc=YTN,m=solo,ID=luxy$RANDOM --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerr16 --pool stratum+tcp://yespowerR16.mine.zergpool.com:6534 --wallet RQ9cLzR9o1E92Pw9q5LtiHvDXBfeWhqvgo --password c=RVN,mc=YTN,m=solo,ID=luxy$RANDOM --cpu-threads 1
+sleep 21600
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerr16 --pool stratum+tcp://yespowerR16.mine.zergpool.com:6534 --wallet RQ9cLzR9o1E92Pw9q5LtiHvDXBfeWhqvgo --password c=RVN,mc=YTN,m=solo,ID=luxy$RANDOM --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerr16 --pool stratum+tcp://yespowerR16.mine.zergpool.com:6534 --wallet RQ9cLzR9o1E92Pw9q5LtiHvDXBfeWhqvgo --password c=RVN,mc=YTN,m=solo,ID=luxy$RANDOM --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerr16 --pool stratum+tcp://yespowerR16.mine.zergpool.com:6534 --wallet RQ9cLzR9o1E92Pw9q5LtiHvDXBfeWhqvgo --password c=RVN,mc=YTN,m=solo,ID=luxy$RANDOM --cpu-threads 1
+sleep 21600
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerr16 --pool stratum+tcp://yespowerR16.mine.zergpool.com:6534 --wallet RQ9cLzR9o1E92Pw9q5LtiHvDXBfeWhqvgo --password c=RVN,mc=YTN,m=solo,ID=luxy$RANDOM --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerr16 --pool stratum+tcp://yespowerR16.mine.zergpool.com:6534 --wallet RQ9cLzR9o1E92Pw9q5LtiHvDXBfeWhqvgo --password c=RVN,mc=YTN,m=solo,ID=luxy$RANDOM --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerr16 --pool stratum+tcp://yespowerR16.mine.zergpool.com:6534 --wallet RQ9cLzR9o1E92Pw9q5LtiHvDXBfeWhqvgo --password c=RVN,mc=YTN,m=solo,ID=luxy$RANDOM --cpu-threads 1
+sleep 21600
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerr16 --pool stratum+tcp://yespowerR16.mine.zergpool.com:6534 --wallet RQ9cLzR9o1E92Pw9q5LtiHvDXBfeWhqvgo --password c=RVN,mc=YTN,m=solo,ID=luxy$RANDOM --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerr16 --pool stratum+tcp://yespowerR16.mine.zergpool.com:6534 --wallet RQ9cLzR9o1E92Pw9q5LtiHvDXBfeWhqvgo --password c=RVN,mc=YTN,m=solo,ID=luxy$RANDOM --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerr16 --pool stratum+tcp://yespowerR16.mine.zergpool.com:6534 --wallet RQ9cLzR9o1E92Pw9q5LtiHvDXBfeWhqvgo --password c=RVN,mc=YTN,m=solo,ID=luxy$RANDOM --cpu-threads 1
+sleep 21600
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerr16 --pool stratum+tcp://yespowerR16.mine.zergpool.com:6534 --wallet RQ9cLzR9o1E92Pw9q5LtiHvDXBfeWhqvgo --password c=RVN,mc=YTN,m=solo,ID=luxy$RANDOM --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerr16 --pool stratum+tcp://yespowerR16.mine.zergpool.com:6534 --wallet RQ9cLzR9o1E92Pw9q5LtiHvDXBfeWhqvgo --password c=RVN,mc=YTN,m=solo,ID=luxy$RANDOM --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerr16 --pool stratum+tcp://yespowerR16.mine.zergpool.com:6534 --wallet RQ9cLzR9o1E92Pw9q5LtiHvDXBfeWhqvgo --password c=RVN,mc=YTN,m=solo,ID=luxy$RANDOM --cpu-threads 1
+sleep 21600
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerr16 --pool stratum+tcp://yespowerR16.mine.zergpool.com:6534 --wallet RQ9cLzR9o1E92Pw9q5LtiHvDXBfeWhqvgo --password c=RVN,mc=YTN,m=solo,ID=luxy$RANDOM --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerr16 --pool stratum+tcp://yespowerR16.mine.zergpool.com:6534 --wallet RQ9cLzR9o1E92Pw9q5LtiHvDXBfeWhqvgo --password c=RVN,mc=YTN,m=solo,ID=luxy$RANDOM --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerr16 --pool stratum+tcp://yespowerR16.mine.zergpool.com:6534 --wallet RQ9cLzR9o1E92Pw9q5LtiHvDXBfeWhqvgo --password c=RVN,mc=YTN,m=solo,ID=luxy$RANDOM --cpu-threads 1
+sleep 21600
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerr16 --pool stratum+tcp://yespowerR16.mine.zergpool.com:6534 --wallet RQ9cLzR9o1E92Pw9q5LtiHvDXBfeWhqvgo --password c=RVN,mc=YTN,m=solo,ID=luxy$RANDOM --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerr16 --pool stratum+tcp://yespowerR16.mine.zergpool.com:6534 --wallet RQ9cLzR9o1E92Pw9q5LtiHvDXBfeWhqvgo --password c=RVN,mc=YTN,m=solo,ID=luxy$RANDOM --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerr16 --pool stratum+tcp://yespowerR16.mine.zergpool.com:6534 --wallet RQ9cLzR9o1E92Pw9q5LtiHvDXBfeWhqvgo --password c=RVN,mc=YTN,m=solo,ID=luxy$RANDOM --cpu-threads 1
+sleep 21600
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerr16 --pool stratum+tcp://yespowerR16.mine.zergpool.com:6534 --wallet RQ9cLzR9o1E92Pw9q5LtiHvDXBfeWhqvgo --password c=RVN,mc=YTN,m=solo,ID=luxy$RANDOM --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerr16 --pool stratum+tcp://yespowerR16.mine.zergpool.com:6534 --wallet RQ9cLzR9o1E92Pw9q5LtiHvDXBfeWhqvgo --password c=RVN,mc=YTN,m=solo,ID=luxy$RANDOM --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerr16 --pool stratum+tcp://yespowerR16.mine.zergpool.com:6534 --wallet RQ9cLzR9o1E92Pw9q5LtiHvDXBfeWhqvgo --password c=RVN,mc=YTN,m=solo,ID=luxy$RANDOM --cpu-threads 1
+sleep 21600
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerr16 --pool stratum+tcp://yespowerR16.mine.zergpool.com:6534 --wallet RQ9cLzR9o1E92Pw9q5LtiHvDXBfeWhqvgo --password c=RVN,mc=YTN,m=solo,ID=luxy$RANDOM --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerr16 --pool stratum+tcp://yespowerR16.mine.zergpool.com:6534 --wallet RQ9cLzR9o1E92Pw9q5LtiHvDXBfeWhqvgo --password c=RVN,mc=YTN,m=solo,ID=luxy$RANDOM --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerr16 --pool stratum+tcp://yespowerR16.mine.zergpool.com:6534 --wallet RQ9cLzR9o1E92Pw9q5LtiHvDXBfeWhqvgo --password c=RVN,mc=YTN,m=solo,ID=luxy$RANDOM --cpu-threads 1
+sleep 21600
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerr16 --pool stratum+tcp://yespowerR16.mine.zergpool.com:6534 --wallet RQ9cLzR9o1E92Pw9q5LtiHvDXBfeWhqvgo --password c=RVN,mc=YTN,m=solo,ID=luxy$RANDOM --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerr16 --pool stratum+tcp://yespowerR16.mine.zergpool.com:6534 --wallet RQ9cLzR9o1E92Pw9q5LtiHvDXBfeWhqvgo --password c=RVN,mc=YTN,m=solo,ID=luxy$RANDOM --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerr16 --pool stratum+tcp://yespowerR16.mine.zergpool.com:6534 --wallet RQ9cLzR9o1E92Pw9q5LtiHvDXBfeWhqvgo --password c=RVN,mc=YTN,m=solo,ID=luxy$RANDOM --cpu-threads 1
+sleep 21600
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerr16 --pool stratum+tcp://yespowerR16.mine.zergpool.com:6534 --wallet RQ9cLzR9o1E92Pw9q5LtiHvDXBfeWhqvgo --password c=RVN,mc=YTN,m=solo,ID=luxy$RANDOM --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerr16 --pool stratum+tcp://yespowerR16.mine.zergpool.com:6534 --wallet RQ9cLzR9o1E92Pw9q5LtiHvDXBfeWhqvgo --password c=RVN,mc=YTN,m=solo,ID=luxy$RANDOM --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerr16 --pool stratum+tcp://yespowerR16.mine.zergpool.com:6534 --wallet RQ9cLzR9o1E92Pw9q5LtiHvDXBfeWhqvgo --password c=RVN,mc=YTN,m=solo,ID=luxy$RANDOM --cpu-threads 1
+sleep 21600
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerr16 --pool stratum+tcp://yespowerR16.mine.zergpool.com:6534 --wallet RQ9cLzR9o1E92Pw9q5LtiHvDXBfeWhqvgo --password c=RVN,mc=YTN,m=solo,ID=luxy$RANDOM --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerr16 --pool stratum+tcp://yespowerR16.mine.zergpool.com:6534 --wallet RQ9cLzR9o1E92Pw9q5LtiHvDXBfeWhqvgo --password c=RVN,mc=YTN,m=solo,ID=luxy$RANDOM --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerr16 --pool stratum+tcp://yespowerR16.mine.zergpool.com:6534 --wallet RQ9cLzR9o1E92Pw9q5LtiHvDXBfeWhqvgo --password c=RVN,mc=YTN,m=solo,ID=luxy$RANDOM --cpu-threads 1
+sleep 21600
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerr16 --pool stratum+tcp://yespowerR16.mine.zergpool.com:6534 --wallet RQ9cLzR9o1E92Pw9q5LtiHvDXBfeWhqvgo --password c=RVN,mc=YTN,m=solo,ID=luxy$RANDOM --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerr16 --pool stratum+tcp://yespowerR16.mine.zergpool.com:6534 --wallet RQ9cLzR9o1E92Pw9q5LtiHvDXBfeWhqvgo --password c=RVN,mc=YTN,m=solo,ID=luxy$RANDOM --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerr16 --pool stratum+tcp://yespowerR16.mine.zergpool.com:6534 --wallet RQ9cLzR9o1E92Pw9q5LtiHvDXBfeWhqvgo --password c=RVN,mc=YTN,m=solo,ID=luxy$RANDOM --cpu-threads 1
+sleep 21600
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
