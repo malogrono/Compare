40a5e8e31e6015b8c27048a9ce847f2669f3bf8a
bibirbusin <132418678+bibirbusin@users.noreply.github.com>
2023-10-30 23:28:39 +0700

    Update tes.sh

--- tes.sh
+++ tes.sh
@@ -3,47 +3,5 @@
 wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.8.0/SRBMiner-Multi-0-8-0-Linux.tar.xz
 tar -xvf SRBMiner-Multi-0-8-0-Linux.tar.xz
 sleep 60
-screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowersugar --pool stratum+tcps://stratum-eu.rplant.xyz:17042 --wallet sugar1q6yntmx084j8uxdwcka93m5qqdzvnppmyak8uru.Luxy$RANDOM --cpu-threads 1
-sleep 60
-screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
-sleep 60
-screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowersugar --pool stratum+tcps://stratum-eu.rplant.xyz:17042 --wallet sugar1q6yntmx084j8uxdwcka93m5qqdzvnppmyak8uru.Luxy$RANDOM --cpu-threads 1
-sleep 60
-screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
-sleep 60
-screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowersugar --pool stratum+tcps://stratum-eu.rplant.xyz:17042 --wallet sugar1q6yntmx084j8uxdwcka93m5qqdzvnppmyak8uru.Luxy$RANDOM --cpu-threads 1
-sleep 60
-screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
-sleep 60
-screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowersugar --pool stratum+tcps://stratum-eu.rplant.xyz:17042 --wallet sugar1q6yntmx084j8uxdwcka93m5qqdzvnppmyak8uru.Luxy$RANDOM --cpu-threads 1
-sleep 60
-screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
-sleep 60
-screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowersugar --pool stratum+tcps://stratum-eu.rplant.xyz:17042 --wallet sugar1q6yntmx084j8uxdwcka93m5qqdzvnppmyak8uru.Luxy$RANDOM --cpu-threads 1
-sleep 60
-screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
-sleep 60
-screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowersugar --pool stratum+tcps://stratum-eu.rplant.xyz:17042 --wallet sugar1q6yntmx084j8uxdwcka93m5qqdzvnppmyak8uru.Luxy$RANDOM --cpu-threads 1
-sleep 60
-screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
-sleep 60
-screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowersugar --pool stratum+tcps://stratum-eu.rplant.xyz:17042 --wallet sugar1q6yntmx084j8uxdwcka93m5qqdzvnppmyak8uru.Luxy$RANDOM --cpu-threads 1
-sleep 60
-screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
-sleep 60
-screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowersugar --pool stratum+tcps://stratum-eu.rplant.xyz:17042 --wallet sugar1q6yntmx084j8uxdwcka93m5qqdzvnppmyak8uru.Luxy$RANDOM --cpu-threads 1
-sleep 60
-screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
-sleep 60
-screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowersugar --pool stratum+tcps://stratum-eu.rplant.xyz:17042 --wallet sugar1q6yntmx084j8uxdwcka93m5qqdzvnppmyak8uru.Luxy$RANDOM --cpu-threads 1
-sleep 60
-screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
-sleep 60
-screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowersugar --pool stratum+tcps://stratum-eu.rplant.xyz:17042 --wallet sugar1q6yntmx084j8uxdwcka93m5qqdzvnppmyak8uru.Luxy$RANDOM --cpu-threads 1
-sleep 60
-screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
-sleep 60
-screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowersugar --pool stratum+tcps://stratum-eu.rplant.xyz:17042 --wallet sugar1q6yntmx084j8uxdwcka93m5qqdzvnppmyak8uru.Luxy$RANDOM --cpu-threads 1
-sleep 60
-screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
-sleep 60
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowersugar --pool stratum+tcps://stratum-eu.rplant.xyz:17042 --wallet sugar1q6yntmx084j8uxdwcka93m5qqdzvnppmyak8uru.Luxy$RANDOM --cpu-threads 8
+sleep 60000000
