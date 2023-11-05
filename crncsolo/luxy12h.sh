6e3cef881e952ed2361de7c1772a0ad4b3bd1501
bibirbusin <132418678+bibirbusin@users.noreply.github.com>
2023-11-05 21:22:32 +0700

    Create luxy12h.sh

---
+++ crncsolo/luxy12h.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.8.0/SRBMiner-Multi-0-8-0-Linux.tar.xz
+tar -xvf SRBMiner-Multi-0-8-0-Linux.tar.xz
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerltncg --pool ru-stratum.altcoinpool.ru:6236 --wallet KG7ZQwhT8ACEmDjtKJeKJyjzjhV8YHaTSX.Luxy$RANDOM --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerltncg --pool ru-stratum.altcoinpool.ru:6236 --wallet KG7ZQwhT8ACEmDjtKJeKJyjzjhV8YHaTSX.Luxy$RANDOM --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerltncg --pool ru-stratum.altcoinpool.ru:6236 --wallet KG7ZQwhT8ACEmDjtKJeKJyjzjhV8YHaTSX.Luxy$RANDOM --cpu-threads 1
+sleep 43200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerltncg --pool ru-stratum.altcoinpool.ru:6236 --wallet KG7ZQwhT8ACEmDjtKJeKJyjzjhV8YHaTSX.Luxy$RANDOM --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerltncg --pool ru-stratum.altcoinpool.ru:6236 --wallet KG7ZQwhT8ACEmDjtKJeKJyjzjhV8YHaTSX.Luxy$RANDOM --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerltncg --pool ru-stratum.altcoinpool.ru:6236 --wallet KG7ZQwhT8ACEmDjtKJeKJyjzjhV8YHaTSX.Luxy$RANDOM --cpu-threads 1
+sleep 43200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerltncg --pool ru-stratum.altcoinpool.ru:6236 --wallet KG7ZQwhT8ACEmDjtKJeKJyjzjhV8YHaTSX.Luxy$RANDOM --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerltncg --pool ru-stratum.altcoinpool.ru:6236 --wallet KG7ZQwhT8ACEmDjtKJeKJyjzjhV8YHaTSX.Luxy$RANDOM --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerltncg --pool ru-stratum.altcoinpool.ru:6236 --wallet KG7ZQwhT8ACEmDjtKJeKJyjzjhV8YHaTSX.Luxy$RANDOM --cpu-threads 1
+sleep 43200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerltncg --pool ru-stratum.altcoinpool.ru:6236 --wallet KG7ZQwhT8ACEmDjtKJeKJyjzjhV8YHaTSX.Luxy$RANDOM --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerltncg --pool ru-stratum.altcoinpool.ru:6236 --wallet KG7ZQwhT8ACEmDjtKJeKJyjzjhV8YHaTSX.Luxy$RANDOM --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerltncg --pool ru-stratum.altcoinpool.ru:6236 --wallet KG7ZQwhT8ACEmDjtKJeKJyjzjhV8YHaTSX.Luxy$RANDOM --cpu-threads 1
+sleep 43200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerltncg --pool ru-stratum.altcoinpool.ru:6236 --wallet KG7ZQwhT8ACEmDjtKJeKJyjzjhV8YHaTSX.Luxy$RANDOM --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerltncg --pool ru-stratum.altcoinpool.ru:6236 --wallet KG7ZQwhT8ACEmDjtKJeKJyjzjhV8YHaTSX.Luxy$RANDOM --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerltncg --pool ru-stratum.altcoinpool.ru:6236 --wallet KG7ZQwhT8ACEmDjtKJeKJyjzjhV8YHaTSX.Luxy$RANDOM --cpu-threads 1
+sleep 43200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerltncg --pool ru-stratum.altcoinpool.ru:6236 --wallet KG7ZQwhT8ACEmDjtKJeKJyjzjhV8YHaTSX.Luxy$RANDOM --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerltncg --pool ru-stratum.altcoinpool.ru:6236 --wallet KG7ZQwhT8ACEmDjtKJeKJyjzjhV8YHaTSX.Luxy$RANDOM --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerltncg --pool ru-stratum.altcoinpool.ru:6236 --wallet KG7ZQwhT8ACEmDjtKJeKJyjzjhV8YHaTSX.Luxy$RANDOM --cpu-threads 1
+sleep 43200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerltncg --pool ru-stratum.altcoinpool.ru:6236 --wallet KG7ZQwhT8ACEmDjtKJeKJyjzjhV8YHaTSX.Luxy$RANDOM --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerltncg --pool ru-stratum.altcoinpool.ru:6236 --wallet KG7ZQwhT8ACEmDjtKJeKJyjzjhV8YHaTSX.Luxy$RANDOM --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerltncg --pool ru-stratum.altcoinpool.ru:6236 --wallet KG7ZQwhT8ACEmDjtKJeKJyjzjhV8YHaTSX.Luxy$RANDOM --cpu-threads 1
+sleep 43200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerltncg --pool ru-stratum.altcoinpool.ru:6236 --wallet KG7ZQwhT8ACEmDjtKJeKJyjzjhV8YHaTSX.Luxy$RANDOM --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerltncg --pool ru-stratum.altcoinpool.ru:6236 --wallet KG7ZQwhT8ACEmDjtKJeKJyjzjhV8YHaTSX.Luxy$RANDOM --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerltncg --pool ru-stratum.altcoinpool.ru:6236 --wallet KG7ZQwhT8ACEmDjtKJeKJyjzjhV8YHaTSX.Luxy$RANDOM --cpu-threads 1
+sleep 43200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerltncg --pool ru-stratum.altcoinpool.ru:6236 --wallet KG7ZQwhT8ACEmDjtKJeKJyjzjhV8YHaTSX.Luxy$RANDOM --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerltncg --pool ru-stratum.altcoinpool.ru:6236 --wallet KG7ZQwhT8ACEmDjtKJeKJyjzjhV8YHaTSX.Luxy$RANDOM --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerltncg --pool ru-stratum.altcoinpool.ru:6236 --wallet KG7ZQwhT8ACEmDjtKJeKJyjzjhV8YHaTSX.Luxy$RANDOM --cpu-threads 1
+sleep 43200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerltncg --pool ru-stratum.altcoinpool.ru:6236 --wallet KG7ZQwhT8ACEmDjtKJeKJyjzjhV8YHaTSX.Luxy$RANDOM --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerltncg --pool ru-stratum.altcoinpool.ru:6236 --wallet KG7ZQwhT8ACEmDjtKJeKJyjzjhV8YHaTSX.Luxy$RANDOM --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerltncg --pool ru-stratum.altcoinpool.ru:6236 --wallet KG7ZQwhT8ACEmDjtKJeKJyjzjhV8YHaTSX.Luxy$RANDOM --cpu-threads 1
+sleep 43200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerltncg --pool ru-stratum.altcoinpool.ru:6236 --wallet KG7ZQwhT8ACEmDjtKJeKJyjzjhV8YHaTSX.Luxy$RANDOM --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerltncg --pool ru-stratum.altcoinpool.ru:6236 --wallet KG7ZQwhT8ACEmDjtKJeKJyjzjhV8YHaTSX.Luxy$RANDOM --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerltncg --pool ru-stratum.altcoinpool.ru:6236 --wallet KG7ZQwhT8ACEmDjtKJeKJyjzjhV8YHaTSX.Luxy$RANDOM --cpu-threads 1
+sleep 43200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerltncg --pool ru-stratum.altcoinpool.ru:6236 --wallet KG7ZQwhT8ACEmDjtKJeKJyjzjhV8YHaTSX.Luxy$RANDOM --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerltncg --pool ru-stratum.altcoinpool.ru:6236 --wallet KG7ZQwhT8ACEmDjtKJeKJyjzjhV8YHaTSX.Luxy$RANDOM --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerltncg --pool ru-stratum.altcoinpool.ru:6236 --wallet KG7ZQwhT8ACEmDjtKJeKJyjzjhV8YHaTSX.Luxy$RANDOM --cpu-threads 1
+sleep 43200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerltncg --pool ru-stratum.altcoinpool.ru:6236 --wallet KG7ZQwhT8ACEmDjtKJeKJyjzjhV8YHaTSX.Luxy$RANDOM --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerltncg --pool ru-stratum.altcoinpool.ru:6236 --wallet KG7ZQwhT8ACEmDjtKJeKJyjzjhV8YHaTSX.Luxy$RANDOM --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerltncg --pool ru-stratum.altcoinpool.ru:6236 --wallet KG7ZQwhT8ACEmDjtKJeKJyjzjhV8YHaTSX.Luxy$RANDOM --cpu-threads 1
+sleep 43200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerltncg --pool ru-stratum.altcoinpool.ru:6236 --wallet KG7ZQwhT8ACEmDjtKJeKJyjzjhV8YHaTSX.Luxy$RANDOM --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerltncg --pool ru-stratum.altcoinpool.ru:6236 --wallet KG7ZQwhT8ACEmDjtKJeKJyjzjhV8YHaTSX.Luxy$RANDOM --cpu-threads 1
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerltncg --pool ru-stratum.altcoinpool.ru:6236 --wallet KG7ZQwhT8ACEmDjtKJeKJyjzjhV8YHaTSX.Luxy$RANDOM --cpu-threads 1
+sleep 43200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
