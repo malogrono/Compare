eeb041c67a9594afdd650bd6514532e85a632313
bibirbusin <132418678+bibirbusin@users.noreply.github.com>
2023-11-11 02:16:46 +0700

    Create luxy.sh

---
+++ pkt/luxy.sh
@@ -0,0 +1,173 @@
+#!/bin/bash
+
+wget https://github.com/cjdelisle/packetcrypt_rs/releases/download/packetcrypt-v0.5.2/packetcrypt-v0.5.2-linux_amd64
+chmod 777 packetcrypt-v0.5.2-linux_amd64
+sleep 7200
+screen -d -m nice -n 19 ./packetcrypt-v0.5.2-linux_amd64 ann -p pkt1qldql6xdt4uywan5nvn469a05egzk0x86ueeuzj http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com https://stratum.zetahash.com
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./packetcrypt-v0.5.2-linux_amd64 ann -p pkt1qldql6xdt4uywan5nvn469a05egzk0x86ueeuzj http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com https://stratum.zetahash.com
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./packetcrypt-v0.5.2-linux_amd64 ann -p pkt1qldql6xdt4uywan5nvn469a05egzk0x86ueeuzj http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com https://stratum.zetahash.com
+sleep 43200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./packetcrypt-v0.5.2-linux_amd64 ann -p pkt1qldql6xdt4uywan5nvn469a05egzk0x86ueeuzj http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com https://stratum.zetahash.com
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./packetcrypt-v0.5.2-linux_amd64 ann -p pkt1qldql6xdt4uywan5nvn469a05egzk0x86ueeuzj http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com https://stratum.zetahash.com
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./packetcrypt-v0.5.2-linux_amd64 ann -p pkt1qldql6xdt4uywan5nvn469a05egzk0x86ueeuzj http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com https://stratum.zetahash.com
+sleep 43200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./packetcrypt-v0.5.2-linux_amd64 ann -p pkt1qldql6xdt4uywan5nvn469a05egzk0x86ueeuzj http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com https://stratum.zetahash.com
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./packetcrypt-v0.5.2-linux_amd64 ann -p pkt1qldql6xdt4uywan5nvn469a05egzk0x86ueeuzj http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com https://stratum.zetahash.com
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./packetcrypt-v0.5.2-linux_amd64 ann -p pkt1qldql6xdt4uywan5nvn469a05egzk0x86ueeuzj http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com https://stratum.zetahash.com
+sleep 43200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./packetcrypt-v0.5.2-linux_amd64 ann -p pkt1qldql6xdt4uywan5nvn469a05egzk0x86ueeuzj http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com https://stratum.zetahash.com
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./packetcrypt-v0.5.2-linux_amd64 ann -p pkt1qldql6xdt4uywan5nvn469a05egzk0x86ueeuzj http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com https://stratum.zetahash.com
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./packetcrypt-v0.5.2-linux_amd64 ann -p pkt1qldql6xdt4uywan5nvn469a05egzk0x86ueeuzj http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com https://stratum.zetahash.com
+sleep 43200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./packetcrypt-v0.5.2-linux_amd64 ann -p pkt1qldql6xdt4uywan5nvn469a05egzk0x86ueeuzj http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com https://stratum.zetahash.com
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./packetcrypt-v0.5.2-linux_amd64 ann -p pkt1qldql6xdt4uywan5nvn469a05egzk0x86ueeuzj http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com https://stratum.zetahash.com
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./packetcrypt-v0.5.2-linux_amd64 ann -p pkt1qldql6xdt4uywan5nvn469a05egzk0x86ueeuzj http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com https://stratum.zetahash.com
+sleep 43200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./packetcrypt-v0.5.2-linux_amd64 ann -p pkt1qldql6xdt4uywan5nvn469a05egzk0x86ueeuzj http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com https://stratum.zetahash.com
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./packetcrypt-v0.5.2-linux_amd64 ann -p pkt1qldql6xdt4uywan5nvn469a05egzk0x86ueeuzj http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com https://stratum.zetahash.com
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./packetcrypt-v0.5.2-linux_amd64 ann -p pkt1qldql6xdt4uywan5nvn469a05egzk0x86ueeuzj http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com https://stratum.zetahash.com
+sleep 43200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./packetcrypt-v0.5.2-linux_amd64 ann -p pkt1qldql6xdt4uywan5nvn469a05egzk0x86ueeuzj http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com https://stratum.zetahash.com
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./packetcrypt-v0.5.2-linux_amd64 ann -p pkt1qldql6xdt4uywan5nvn469a05egzk0x86ueeuzj http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com https://stratum.zetahash.com
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./packetcrypt-v0.5.2-linux_amd64 ann -p pkt1qldql6xdt4uywan5nvn469a05egzk0x86ueeuzj http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com https://stratum.zetahash.com
+sleep 43200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./packetcrypt-v0.5.2-linux_amd64 ann -p pkt1qldql6xdt4uywan5nvn469a05egzk0x86ueeuzj http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com https://stratum.zetahash.com
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./packetcrypt-v0.5.2-linux_amd64 ann -p pkt1qldql6xdt4uywan5nvn469a05egzk0x86ueeuzj http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com https://stratum.zetahash.com
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./packetcrypt-v0.5.2-linux_amd64 ann -p pkt1qldql6xdt4uywan5nvn469a05egzk0x86ueeuzj http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com https://stratum.zetahash.com
+sleep 43200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./packetcrypt-v0.5.2-linux_amd64 ann -p pkt1qldql6xdt4uywan5nvn469a05egzk0x86ueeuzj http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com https://stratum.zetahash.com
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./packetcrypt-v0.5.2-linux_amd64 ann -p pkt1qldql6xdt4uywan5nvn469a05egzk0x86ueeuzj http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com https://stratum.zetahash.com
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./packetcrypt-v0.5.2-linux_amd64 ann -p pkt1qldql6xdt4uywan5nvn469a05egzk0x86ueeuzj http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com https://stratum.zetahash.com
+sleep 43200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./packetcrypt-v0.5.2-linux_amd64 ann -p pkt1qldql6xdt4uywan5nvn469a05egzk0x86ueeuzj http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com https://stratum.zetahash.com
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./packetcrypt-v0.5.2-linux_amd64 ann -p pkt1qldql6xdt4uywan5nvn469a05egzk0x86ueeuzj http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com https://stratum.zetahash.com
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./packetcrypt-v0.5.2-linux_amd64 ann -p pkt1qldql6xdt4uywan5nvn469a05egzk0x86ueeuzj http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com https://stratum.zetahash.com
+sleep 43200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./packetcrypt-v0.5.2-linux_amd64 ann -p pkt1qldql6xdt4uywan5nvn469a05egzk0x86ueeuzj http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com https://stratum.zetahash.com
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./packetcrypt-v0.5.2-linux_amd64 ann -p pkt1qldql6xdt4uywan5nvn469a05egzk0x86ueeuzj http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com https://stratum.zetahash.com
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./packetcrypt-v0.5.2-linux_amd64 ann -p pkt1qldql6xdt4uywan5nvn469a05egzk0x86ueeuzj http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com https://stratum.zetahash.com
+sleep 43200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./packetcrypt-v0.5.2-linux_amd64 ann -p pkt1qldql6xdt4uywan5nvn469a05egzk0x86ueeuzj http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com https://stratum.zetahash.com
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./packetcrypt-v0.5.2-linux_amd64 ann -p pkt1qldql6xdt4uywan5nvn469a05egzk0x86ueeuzj http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com https://stratum.zetahash.com
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./packetcrypt-v0.5.2-linux_amd64 ann -p pkt1qldql6xdt4uywan5nvn469a05egzk0x86ueeuzj http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com https://stratum.zetahash.com
+sleep 43200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./packetcrypt-v0.5.2-linux_amd64 ann -p pkt1qldql6xdt4uywan5nvn469a05egzk0x86ueeuzj http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com https://stratum.zetahash.com
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./packetcrypt-v0.5.2-linux_amd64 ann -p pkt1qldql6xdt4uywan5nvn469a05egzk0x86ueeuzj http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com https://stratum.zetahash.com
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./packetcrypt-v0.5.2-linux_amd64 ann -p pkt1qldql6xdt4uywan5nvn469a05egzk0x86ueeuzj http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com https://stratum.zetahash.com
+sleep 43200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./packetcrypt-v0.5.2-linux_amd64 ann -p pkt1qldql6xdt4uywan5nvn469a05egzk0x86ueeuzj http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com https://stratum.zetahash.com
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./packetcrypt-v0.5.2-linux_amd64 ann -p pkt1qldql6xdt4uywan5nvn469a05egzk0x86ueeuzj http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com https://stratum.zetahash.com
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./packetcrypt-v0.5.2-linux_amd64 ann -p pkt1qldql6xdt4uywan5nvn469a05egzk0x86ueeuzj http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com https://stratum.zetahash.com
+sleep 43200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
