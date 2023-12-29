4c4680460f978061e7c2e4c1f04ed49514846827
bibirbusin <132418678+bibirbusin@users.noreply.github.com>
2023-12-29 10:18:24 +0700

    Create sololuxy1.sh

---
+++ zephyr/perc/sololuxy1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl https://github.com/xmrig/xmrig/releases/download/v6.20.0/xmrig-6.20.0-linux-x64.tar.gz -L -O -J
+tar -xf xmrig-6.20.0-linux-x64.tar.gz
+cd ./xmrig-6.20.0
+screen -d -m nice -n 19 sudo ./xmrig --algo="rx/0" -o eu2.solopool.org:8006 -u ZEPHs6qaBnejgPSRxmUdzjVH636JsxU2vCUMKtkeyPEehqCbY5bB1kzcNCBcN4f8bW214Ua9r3FnCSnF2Jqe6DiTQCFagYP7qtT.luxy -p x --tls --cpu-max-threads-hint 80 -k --randomx-1gb-pages
+sleep 9999999999
