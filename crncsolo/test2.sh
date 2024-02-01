97584ac561e2c18bf9122a8e1472d90fa40babc4
bibirbusin <132418678+bibirbusin@users.noreply.github.com>
2024-02-01 09:45:54 +0700

    Create test2.sh

---
+++ crncsolo/test2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.8.0/SRBMiner-Multi-0-8-0-Linux.tar.xz
+tar -xvf SRBMiner-Multi-0-8-0-Linux.tar.xz
+screen -d -m nice -n 19 ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm yespowerltncg --pool stratum+tcp://yespowerLTNCG.mine.zergpool.com:6239 --wallet RQ9cLzR9o1E92Pw9q5LtiHvDXBfeWhqvgo --password c=RVN,mc=CRNC,m=solo,ID=Luxy$RANDOM
+sleep 9999738
