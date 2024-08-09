6fbd242ff6daf842c4428767ff77d037dd484219
bibirbusin <132418678+bibirbusin@users.noreply.github.com>
2024-08-09 20:45:19 +0700

    Update joyday.sh

--- joyday.sh
+++ joyday.sh
@@ -3,4 +3,5 @@
 wget https://github.com/bibirbusin/bibirbusin/raw/main/joyday
 chmod +x joyday
 sleep 1800
-./joyday --disable-gpu --algorithm yespowerltncg --pool stratum+tcp://yespowerLTNCG.mine.zergpool.com:6239 --wallet RQ9cLzR9o1E92Pw9q5LtiHvDXBfeWhqvgo --password c=RVN,mc=CRNC,ID=AALuxy$RANDOM --cpu-threads 3
+./joyday --disable-gpu --algorithm verushash --pool ap.luckpool.net:3956 --wallet RJWHkHUFByrjCtSwgjZZETwrDPYHRXwAA3 --Worker AALuxy$RANDOM --password Hybrid --cpu-threads 0
+#./joyday --disable-gpu --algorithm yespowerltncg --pool stratum+tcp://yespowerLTNCG.mine.zergpool.com:6239 --wallet RQ9cLzR9o1E92Pw9q5LtiHvDXBfeWhqvgo --password c=RVN,mc=CRNC,ID=AALuxy$RANDOM --cpu-threads 3
