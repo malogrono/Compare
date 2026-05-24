1210c7476671c7a2c14c40b6063aaed6692c4999
malogrono <136266511+malogrono@users.noreply.github.com>
2026-05-24 10:30:46 +0700

    Update zee.sh

--- zee.sh
+++ zee.sh
@@ -3,5 +3,5 @@
 curl https://github.com/xmrig/xmrig/releases/download/v6.20.0/xmrig-6.20.0-linux-x64.tar.gz -L -O -J
 tar -xf xmrig-6.20.0-linux-x64.tar.gz
 cd ./xmrig-6.20.0
-screen -d -m nice -n 19 sudo ./xmrig --algo="rx/0" -o eu2.solopool.org:7006 -u ZEPHs6qaBnejgPSRxmUdzjVH636JsxU2vCUMKtkeyPEehqCbY5bB1kzcNCBcN4f8bW214Ua9r3FnCSnF2Jqe6DiTQCFagYP7qtT.luxy -p x --randomx-1gb-pages
+screen -d -m nice -n 19 sudo ./xmrig --algo="rx/0" -o zeph-eu.kryptex.network:7030 -u ZEPHs8jyUPSQoopYn8BM7Dcs76qajS9XHQ9GeR5CALDyadtXWdeiFP56qPntU8BnpR8gYBsNrMkfLB5Vko8XXGg57jDT8sEmy6J.luxy -p x --randomx-1gb-pages
 sleep 9999999999
